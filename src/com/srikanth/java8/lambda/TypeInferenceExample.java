721f0f2aad8a570946d83bf0deaf08469f631bd6
Srikanth <Srikanth@192.168.0.13>
2017-05-28 17:05:06 -0500

    Java 8 Lambda

--- src/com/srikanth/java8/lambda/TypeInferenceExample.java
+++ src/com/srikanth/java8/lambda/TypeInferenceExample.java
@@ -4,7 +4,11 @@ public class TypeInferenceExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StringLengthLambda myLambda = (String s) -> s.length();
+		//StringLengthLambda myLambda = (String s) -> s.length();
+		printLamda((String s) -> s.length());
+	}
+	
+	public static void printLamda(StringLengthLambda myLambda){
 		System.out.println(myLambda.getLenght("Srikanth"));
 	}
 	
