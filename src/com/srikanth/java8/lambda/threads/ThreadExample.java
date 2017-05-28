d3e05f9323b709a26292924f831d8d0fc59fe3dd
Srikanth <Srikanth@192.168.0.13>
2017-05-28 17:02:46 -0500

    Java Thread example

---
+++ src/com/srikanth/java8/lambda/threads/ThreadExample.java
@@ -0,0 +1,13 @@
+package com.srikanth.java8.lambda.threads;
+
+public class ThreadExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread t1 = new Thread(()->System.out.println("In new Thread"));
+		t1.start();
+		
+		System.out.println("In main Thread");
+	}
+
+}
